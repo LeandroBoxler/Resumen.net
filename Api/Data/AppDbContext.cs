444646d0a6dcacfc583418248269d33b36a9ece3
Leandro Boxler <leandroboxler@gmail.com>
2026-02-14 01:46:08 -0300

    feat: agregar entidad User y migraciones correspondientes

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace Api.Data;
 public class AppDbContext : DbContext
 {
     public DbSet<StudyNote> StudyNotes { get; set; }
+    public DbSet<User> Users { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
@@ -18,6 +19,15 @@ public class AppDbContext : DbContext
         {
             eb.HasKey(e => e.Id);
             eb.Property(e => e.Name).IsRequired();
+        });
+
+        modelBuilder.Entity<User>(eb =>
+        {
+            eb.HasKey(e => e.Id);
+            eb.Property(e => e.Email).IsRequired();
+            eb.Property(e => e.FirstName).IsRequired();
+            eb.Property(e => e.LastName).IsRequired();
+            eb.Property(e => e.HashedPassword).IsRequired();
         });
     }
 }
