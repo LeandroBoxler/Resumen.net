2ea836782cbd9f2330cd824ab174b4d0339908cb
Leandro Boxler <leandroboxler@gmail.com>
2026-02-14 01:45:54 -0300

    feat: agregar autenticación JWT y configuración de servicios relacionados

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,15 +3,18 @@ using Api.Data;
 using domain.services;
 using domain.Entities;
 using Api.Services;
-
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=Data/data.db"));
 
-builder.Services.AddScoped<StudyNotesServices<StudyNote>, PostgreStudyNotesService>();
+builder.Services.AddScoped<IService<StudyNote>, PostgreStudyNotesService>();
+builder.Services.AddScoped<IService<User>, PostgreUsersService>();
+builder.Services.AddScoped<ICryptoService, CryptoService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,6 +30,24 @@ builder.Services.AddCors(options =>
         });
 });
 
+var jwtSecret =builder.Configuration["JwtSecret"] ?? throw new Exception("JwtSecret not found in configuration");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    
+options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret!)),
+        ClockSkew = TimeSpan.Zero
+    };});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -39,6 +60,7 @@ app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
